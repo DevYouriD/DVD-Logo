3479dd7401e1dbb4f94436621d702368bb67dcd8
DevYouriD <youri.dera@gmail.com>
2022-04-03 16:35:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const section = document.querySelector('section');
+const logo = document.querySelector('.logo');
+const FPS = 100;
+
+section.style.height = window.innerHeight + 'px';
+section.style.width = window.innerWidth + 'px';
+
+let xPosition = 10;
+let yPosition = 10;
+let xSpeed = 4;
+let ySpeed = 4;
+
+function update(){
+    logo.style.left = xPosition + 'px';
+    logo.style.top = yPosition + 'px';
+}
+
+
+setInterval(() => {
+    if(xPosition + logo.clientWidth >= window.innerWidth || xPosition <= 0){
+        logo.style.fill = randomColor()
+        xSpeed = -xSpeed;
+    }
+    if(yPosition + logo.clientHeight >= window.innerHeight || yPosition <= 0){
+        logo.style.fill = randomColor()
+        ySpeed = -ySpeed;
+    }
+    xPosition += xSpeed;
+    yPosition += ySpeed;
+    update();
+}, 1000/FPS);
+
+function randomColor(){
+    let color = '#';
+    color += Math.random().toString(16).slice(2,8).toUpperCase();
+    return color;
+}
+
+window.addEventListener('resize', ()=>{
+    xPosition = 10;
+    yPosition = 10;
+    section.style.height = window.innerHeight + 'px';
+    section.style.width = window.innerWidth + 'px';
+})
